1066a5cc9e796738f8e388d7b73f611df41399fe
danielmenezesbr <danielmenezes@gmail.com>
2023-11-27 17:05:37 -0300

    change postgresql to oracle xe

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (name, email) VALUES
+    ('Primeiro Cliente', 'primeiro_cliente@example.com');
+
+INSERT INTO customers (name, email) VALUES
+    ('Segundo Cliente', 'segundo_cliente@example.com');
--- src/test/java/com/testcontainers/demo/CustomerControllerTest.java
+++ src/test/java/com/testcontainers/demo/CustomerControllerTest.java
@@ -15,33 +15,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.OracleContainer;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CustomerControllerTest {
+@SpringBootTest(
+        classes = SpringBootRestApiTestingGuideApplication.class,
+        webEnvironment = WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles("test")
 
+public class CustomerControllerTest {
   @LocalServerPort
   private Integer port;
 
-  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-    "postgres:15-alpine"
-  );
+  static OracleContainer db = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+          .withDatabaseName("testDB")
+          .withUsername("testUser")
+          .withPassword("testPassword");
 
   @BeforeAll
   static void beforeAll() {
-    postgres.start();
+    db.start();
   }
 
   @AfterAll
   static void afterAll() {
-    postgres.stop();
+    db.stop();
   }
 
   @DynamicPropertySource
   static void configureProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.datasource.url", db::getJdbcUrl);
+    registry.add("spring.datasource.username", db::getUsername);
+    registry.add("spring.datasource.password", db::getPassword);
   }
 
   @Autowired
@@ -50,23 +57,24 @@ class CustomerControllerTest {
   @BeforeEach
   void setUp() {
     RestAssured.baseURI = "http://localhost:" + port;
-    customerRepository.deleteAll();
   }
 
   @Test
   void shouldGetAllCustomers() {
     List<Customer> customers = List.of(
-      new Customer(null, "John", "john@mail.com"),
-      new Customer(null, "Dennis", "dennis@mail.com")
+            new Customer(null, "John", "john@mail.com"),
+            new Customer(null, "Dennis", "dennis@mail.com")
     );
     customerRepository.saveAll(customers);
 
     given()
-      .contentType(ContentType.JSON)
-      .when()
-      .get("/api/customers")
-      .then()
-      .statusCode(200)
-      .body(".", hasSize(2));
+            .contentType(ContentType.JSON)
+            .when()
+            .get("/api/customers")
+            .then()
+            .statusCode(200)
+            .body(".", hasSize(4));
   }
+
 }
+
